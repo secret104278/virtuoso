9b6fef4e1ca3dd2c67720d42e480bfef8cb8f63a
secret104278 <secret104278@gmail.com>
2025-12-13 09:03:59 +0800

    feat: Add music theory constants for note mappings, scale intervals, and cadences.

---
+++ src/lib/music-theory.ts
@@ -0,0 +1,60 @@
+const xxx = {
+	1: "C",
+	2: "^C",
+	3: "D",
+	4: "^D",
+	5: "E",
+	6: "F",
+	7: "^F",
+	8: "G",
+	9: "^G",
+	10: "A",
+	11: "^A",
+	12: "B",
+};
+
+const yyy = {
+	1: "C",
+	2: "_D",
+	3: "D",
+	4: "_E",
+	5: "E",
+	6: "F",
+	7: "_G",
+	8: "G",
+	9: "_A",
+	10: "A",
+	11: "_B",
+	12: "B",
+};
+
+const majorUp = [2, 2, 1, 2, 2, 2, 1];
+const majorDown = [1, 2, 2, 1, 2, 2, 2];
+const minorHarmonicUp = [2, 1, 2, 2, 1, 3, 1];
+const minorHarmonicDown = [1, 3, 1, 2, 2, 1, 2];
+const minorMelodicUp = [2, 1, 2, 2, 1, 2, 2];
+const minorMelodicDown = [2, 2, 1, 2, 2, 1, 2];
+
+const cadencesLeftHand = [[-12], [5, -7], [7, -5], [-5, -17], [0, -12]];
+const cadencesRightHand = [
+	[0],
+	[2, 9, 12],
+	[4, 7, 12],
+	[2, 5, 7, 11],
+	[0, 4, 7, 12],
+];
+
+const scales = [
+	"C",
+	"F",
+	"_B",
+	"_E",
+	"_A",
+	"_D",
+	"_G",
+	"B",
+	"E",
+	"A",
+	"D",
+	"G",
+];
